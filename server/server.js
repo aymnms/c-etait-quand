3828853fcd468afe92a1fc9e3610ae40a049fbe1
Aymerick Michelet <micheletaymerick@gmail.com>
2025-02-05 15:35:58 +0100

    feat: setup jest #37 #10

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 require("dotenv").config();
-const Room = require("./models/Room");
 const Game = require("./models/Game");
 
 const app = express();
