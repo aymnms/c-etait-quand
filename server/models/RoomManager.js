b81d5017d1a5d6963b9a408c07633007acc0dba1
Aymerick Michelet <micheletaymerick@gmail.com>
2025-02-07 18:24:18 +0100

    feat: add image in question #49

--- server/models/RoomManager.js
+++ server/models/RoomManager.js
@@ -4,9 +4,24 @@ class RoomManager {
     constructor() {
         this.rooms = new Map();
         this.questions = [
-            { invention: "Imprimerie", year: 1440, explanation: "Inventée par Gutenberg." },
-            { invention: "Téléphone", year: 1876, explanation: "Alexander Graham Bell en est l'inventeur." },
-            { invention: "Internet", year: 1969, explanation: "ARPANET, ancêtre d'Internet, a vu le jour en 1969." }
+            {
+                invention: "Imprimerie",
+                image: "https://www.entrepreneur-individuel.fr/wp-content/uploads/2022/06/imprimeur.jpg",
+                year: 1440,
+                explanation: "Inventée par Gutenberg."
+            },
+            {
+                invention: "Téléphone",
+                image: "https://cdn.futura-sciences.com/cdn-cgi/image/width=1920,quality=50,format=auto/sources/images/dossier/1944/portable2.jpg",
+                year: 1876,
+                explanation: "Alexander Graham Bell en est l'inventeur."
+            },
+            {
+                invention: "Internet",
+                image: "https://cdn.futura-sciences.com/cdn-cgi/image/width=1920,quality=50,format=auto/sources/images/actu/icann-internet-nom-de-domaine.jpg",
+                year: 1969,
+                explanation: "ARPANET, ancêtre d'Internet, a vu le jour en 1969."
+            }
         ];
     }
 
